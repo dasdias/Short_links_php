238511d132fdc9cf5ce053465f3a7469b8bbb814
Alex Fix <dasdias@yandex.ru>
2021-10-26 22:53:39 +0300

    add dump sql base

---
+++ db/links.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 26 2021 г., 22:51
+-- Версия сервера: 8.0.15
+-- Версия PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `cut_url`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `links`
+--
+
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `long_link` varchar(250) DEFAULT NULL,
+  `short_link` varchar(20) DEFAULT NULL,
+  `views` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `links`
+--
+
+INSERT INTO `links` (`id`, `user_id`, `long_link`, `short_link`, `views`) VALUES
+(1, 1, 'https://yandex.ru', 'https://ya.ru', 0),
+(2, 1, 'https://yandex.ru', 'https://ya.ru', 0);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `links`
+--
+ALTER TABLE `links`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `links`
+--
+ALTER TABLE `links`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
